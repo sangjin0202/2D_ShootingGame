b049045e6715974efc4aa52f3270b863aae73d4b
sangjin0202 <sangjin202@naver.com>
2023-10-04 17:09:42 +0900

    # 프로젝트 백업

---
+++ Assets/Scripts/Explosion.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+    Animator anim;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+	void OnEnable()
+	{
+        Invoke("Disble", 2f);
+	}
+
+    void Disble()
+	{
+        gameObject.SetActive(false);
+	}
+
+	public void StartExplosion(string target)
+    {
+        anim.SetTrigger("OnExplosion");
+
+        switch (target)
+		{
+            case "S":
+                transform.localScale = Vector3.one * 0.7f;
+                break;
+            case "M":
+                transform.localScale = Vector3.one * 1;
+                break;
+            case "P":
+                transform.localScale = Vector3.one * 1;
+                break;
+            case "L":
+                transform.localScale = Vector3.one * 2f;
+                break;
+            case "B":
+                transform.localScale = Vector3.one * 3f;
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,233 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.IO;
+
+public class GameManager : MonoBehaviour
+{
+    public int stage;
+    public Animator stageAnim;
+    public Animator claerAnim;
+    public Animator FadeAnim;
+    public Transform playerPos;
+
+    public string[] enemyObjs;
+    public Transform[] spawnPoints;
+
+    public float nextSpawnDelay;
+    public float curSpawnDelay;
+
+    public GameObject player;
+    public Text scoreText;
+    public Image[] lifeImage;
+    public Image[] boomImage;
+    public GameObject gameOverSet;
+    public ObjectManager objectManager;
+
+    public List<Spawn> spawnList;
+    public int spawnIndex;
+    public bool spawnEnd;
+
+    void Awake()
+    {
+        spawnList = new List<Spawn>();
+        enemyObjs = new string[] { "EnemyS", "EnemyM", "EnemyL", "EnemyB" };
+        StageStart();
+    }
+    
+    public void StageStart()
+	{
+        //#.Stage UI Load
+        stageAnim.SetTrigger("On");
+        stageAnim.GetComponent<Text>().text = "STAGE " + stage;
+
+        //#.Enemy SpWn File Read
+        ReadSpawnFile();
+
+        //#.Fade In
+        FadeAnim.SetTrigger("In");
+    }
+
+    public void StageEnd()
+	{
+        //#.Clear UI Load
+        claerAnim.GetComponent<Text>().text = "CLEAR!";
+        claerAnim.SetTrigger("On");
+
+        //#.Fade Out
+        FadeAnim.SetTrigger("Out");
+
+        //#.Player Repos
+        //player.transform.position = playerPos.position;
+
+        //#.Stage Increament
+        stage++;
+        if (stage > 2)
+            Invoke("GameOver",7);
+        else
+            Invoke("StageStart", 5);
+    }
+
+    void ReadSpawnFile()
+    {
+        //#.1 변수초기화
+        spawnList.Clear();
+        spawnIndex = 0;
+        spawnEnd = false;
+
+        //#.2 리스폰 파일 읽기
+        TextAsset textFile = Resources.Load("Stage "+ stage) as TextAsset;
+        StringReader stringReader = new StringReader(textFile.text);
+
+        while(stringReader != null)
+        {
+            string line = stringReader.ReadLine();
+            Debug.Log(line);
+
+            if (line == null)
+                break;
+            //#. 리스폰 데이터 생성
+            Spawn spawnData = new Spawn();
+            spawnData.delay = float.Parse(line.Split(',')[0]);
+            spawnData.type = line.Split(',')[1];
+            spawnData.point = int.Parse(line.Split(',')[2]);
+            spawnList.Add(spawnData);
+        }
+        //#. 텍스트 파일 닫기
+        stringReader.Close();
+
+        //#. 첫번쨰 스폰 딜레이 적용
+        nextSpawnDelay = spawnList[0].delay;
+    }
+
+    void Update()
+    {
+        curSpawnDelay += Time.deltaTime;
+
+        if(curSpawnDelay > nextSpawnDelay && !spawnEnd)
+        {
+            SpawnEnemy();
+            curSpawnDelay = 0;
+        }
+
+        //#.UI Score Update
+        Player playerLogic = player.GetComponent<Player>();
+        scoreText.text = string.Format("{0:n0}", playerLogic.score); 
+    }
+
+    void SpawnEnemy()
+    {
+        int enemyIndex = 0;
+        switch(spawnList[spawnIndex].type)
+        {
+            case "S":
+                enemyIndex = 0;
+                break;
+            case "M":
+                enemyIndex = 1;
+                break;
+            case "L":
+                enemyIndex = 2;
+                break;
+            case "B":
+                enemyIndex = 3;
+                break;
+        }
+        int enemyPoint = spawnList[spawnIndex].point;
+
+        // Pool
+        GameObject enemy = objectManager.MakeObj(enemyObjs[enemyIndex]);
+        enemy.transform.position = spawnPoints[enemyPoint].position;
+
+        Rigidbody2D rigid = enemy.GetComponent<Rigidbody2D>();
+        Enemy enemyLogic = enemy.GetComponent<Enemy>();
+        enemyLogic.player = player;
+        enemyLogic.gamemanager = this;
+        enemyLogic.objectManager = objectManager;
+        if(enemyPoint == 5 || enemyPoint == 6)
+        {
+            enemy.transform.Rotate(Vector3.back * 45);
+            rigid.velocity = new Vector2(enemyLogic.speed* (-1), -1);
+        }
+        else if (enemyPoint == 7 || enemyPoint == 8)
+        {
+            enemy.transform.Rotate(Vector3.forward * 45);
+            rigid.velocity = new Vector2(enemyLogic.speed, -1);
+        }
+        else
+        {
+            rigid.velocity = new Vector2(0, enemyLogic.speed * (-1));
+        }
+
+        //#. 리스폰 인덱스 증가
+        spawnIndex++;
+        if(spawnIndex == spawnList.Count)
+        {
+            spawnEnd = true;
+            return;
+        }
+        //#. 다음 리스폰 딜레이 갱신
+        nextSpawnDelay = spawnList[spawnIndex].delay;
+    }
+
+    public void UpdateLifeIcon(int life)
+    {
+        for (int index = 0; index < 3; index++)
+        {
+            lifeImage[index].color = new Color(1, 1, 1, 0);
+        }
+
+        for (int index= 0; index < life; index++)
+        {
+            lifeImage[index].color = new Color(1, 1, 1, 1);
+        }
+    }
+    
+    public void UpdateBoomIcon(int boom)
+    {
+        for (int index = 0; index < 3; index++)
+        {
+            boomImage[index].color = new Color(1, 1, 1, 0);
+        }
+
+        for (int index= 0; index < boom; index++)
+        {
+            boomImage[index].color = new Color(1, 1, 1, 1);
+        }
+    }
+
+    public void RespawnPlayer()
+    {
+        Invoke("RespawnPlayerExe", 2f);
+    }
+    
+    void RespawnPlayerExe()
+    {
+        player.transform.position = Vector3.down * 4f;
+        player.SetActive(true);
+
+        Player playerLogic = player.GetComponent<Player>();
+        playerLogic.isHit = false;
+    }
+
+    public void CallExplosion(Vector3 pos, string type)
+	{
+        GameObject explosion = objectManager.MakeObj("Explosion");
+        Explosion explosionLogic = explosion.GetComponent<Explosion>();
+
+        explosion.transform.position = pos;
+        explosionLogic.StartExplosion(type);
+	}
+
+    public void GameOver()
+    {
+        gameOverSet.SetActive(true);
+    }
+
+    public void GameRetry()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
